9ac37db1da3954991849416d1a636538164ae315
technology512 <ald.wapper@gmail.com>
2021-03-12 02:19:57 +0100

    declare string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    var myFirstName = "Adel";
+    var myLastName = "Hokulic";
+    var myHomeTown = "Zivinice";
+    return {
+      myFirstName,
+      myLastName,
+      myHomeTown,
+    };
+  }
+  console.log(main());
+  module.exports = main;

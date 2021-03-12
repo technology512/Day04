ad75f5e0adc00f1962ed509ae54773c586542866
technology512 <ald.wapper@gmail.com>
2021-03-12 02:15:06 +0100

    finding a remainder

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+    var remainder;
+    remainder = numOne % numTwo;
+    // Only change code above this line
+    return remainder;
+    }
+
+console.log(main(102, 25)); 
+module.exports = main; 

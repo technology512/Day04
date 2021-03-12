b3b5559da74cf15d0b93975a557c027d7ecc97a4
technology512 <ald.wapper@gmail.com>
2021-03-12 02:09:35 +0100

    decimal numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    var myDecimal = 8.2;
+    var myDecimalE = 0.00254;
+    return {
+      myDecimal,
+      myDecimalE,
+    };
+  }
+  console.log(main());
+  module.exports = main;

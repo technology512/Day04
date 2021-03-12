41314f3ddeb62f44e675d0eacff5a1e7f61d1013
technology512 <ald.wapper@gmail.com>
2021-03-12 02:13:13 +0100

    divide one decimal by another with JS

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+function main(numOne, numTwo) {
+    var myQuotient;
+    myQuotient = numOne / numTwo;
+    return myQuotient;
+  }
+  console.log(main(5.0, 2.0));
+  console.log(main(16.5, 5.5));
+  console.log(main(102.0, 25.5));
+  module.exports = main;
